544d57c60cb283040b695dc224865a792be57080
zapperllp <43103958+zapperllp@users.noreply.github.com>
2020-11-27 22:13:52 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -3,14 +3,9 @@ a = int(input('Number:\n'))
 
 def fac(n):
 
-    if n > 0:
-        return(n*fac(n-1))
+    if n > 0: return(n*fac(n-1))
 
-    else:
-        return 1
+    else: return 1
 
 
 print('The factorial of {} is equal to {}'.format(a, fac(a)))
-
-
-# testing if works
