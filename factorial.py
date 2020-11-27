d66214441cd6107fb9f452f72e4ad27ee0ce526f
zapperllp <krprem514@gmail.com>
2020-11-27 16:41:11 +0530

    prem

---
+++ factorial.py
@@ -0,0 +1,16 @@
+a = int(input('Number:\n'))
+
+
+def fac(n):
+
+    if n > 0:
+        return(n*fac(n-1))
+
+    else:
+        return 1
+
+
+print('The factorial of {} is equal to {}'.format(a, fac(a)))
+
+
+# testing if works
