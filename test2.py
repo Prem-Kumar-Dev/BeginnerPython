1a08572100663e797f2ba48542192e3132adb678
zapperllp <krprem514@gmail.com>
2023-11-07 21:57:59 +0530

    ll

--- test2.py
+++ test2.py
@@ -1,18 +1,3 @@
-a=10
-for i in range(0,a):
-    for k in range(i,a-1):
-        print(" ",end="")
 
-    for j in range(0,i+1):
-        print("* ",end="")
-    
-    print()
 
-for i in range (a-1,0,-1):
-    for k in range(i,a):
-        print(" ",end="")
-    
-    for j in range(i,0,-1):
-        print("* ",end="")
-    print()
 
